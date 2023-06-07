39009f6b92c7b5f24d29b87a32549363d1f5dc9f
rootaccess2023 <134943577+rootaccess2023@users.noreply.github.com>
2023-06-07 21:46:51 +0800

    Update index.js

--- express_exercise/index.js
+++ express_exercise/index.js
@@ -1,6 +1,10 @@
-const express = require('express' );
+const express = require("express");
 const app = express();
-const port = 8000;
+const port = 3000;
+
+app.get("/", (req, res) => {
+    console.log(`New request made: ${req.method}`);
+});
 
 app.listen(port, () => {
     console.log(`The server is listening on port ${port}`)
